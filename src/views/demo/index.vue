c651a08e9bd97dd1d38a195dabc88955d180579c
郑先生 <1160206165@qq.com>
2022-10-28 18:57:48 +0800

    feat: 删除多余的文件和代码 & 添加目录结构 & 重构 main.ts & 初始化样式 & 定义 Demo 路由

---
+++ src/views/demo/index.vue
@@ -0,0 +1,13 @@
+<template>
+  demo
+</template>
+<script lang='ts'>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'demo',
+  setup() {
+    return {}
+  }
+})
+</script>
+<style lang='scss' scoped></style>
